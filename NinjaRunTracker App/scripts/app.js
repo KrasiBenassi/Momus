97e3145a24825b4ae380ff58f321357e9e2d257f
Miroslav Vitalievich <miroslavgatsanoga@gmail.com>
2014-09-28 07:34:53 -0500

    Changes made by Miroslav Vitalievich (miroslavgatsanoga@gmail.com).

--- NinjaRunTracker App/scripts/app.js
+++ NinjaRunTracker App/scripts/app.js
@@ -18,7 +18,12 @@
                         $("#home_network_button").text('Internet access enabled');
                     }
                 },
-
+                clearLocalStorage: function () {
+                    window.localStorage.clear();
+                },
+                seedGpsData: function () {
+                    window.localStorage.setItem('Sample block', '[{"timestamp":1335700802000,"coords":{"heading":null,"altitude":null,"longitude":170.33488333333335,"accuracy":0,"latitude":-45.87475166666666,"speed":null,"altitudeAccuracy":null}},{"timestamp":1335700803000,"coords":{"heading":null,"altitude":null,"longitude":170.33481666666665,"accuracy":0,"latitude":-45.87465,"speed":null,"altitudeAccuracy":null}},{"timestamp":1335700804000,"coords":{"heading":null,"altitude":null,"longitude":170.33426999999998,"accuracy":0,"latitude":-45.873708333333326,"speed":null,"altitudeAccuracy":null}},{"timestamp":1335700805000,"coords":{"heading":null,"altitude":null,"longitude":170.33318333333335,"accuracy":0,"latitude":-45.87178333333333,"speed":null,"altitudeAccuracy":null}},{"timestamp":1335700806000,"coords":{"heading":null,"altitude":null,"longitude":170.33416166666666,"accuracy":0,"latitude":-45.871478333333336,"speed":null,"altitudeAccuracy":null}},{"timestamp":1335700807000,"coords":{"heading":null,"altitude":null,"longitude":170.33526833333332,"accuracy":0,"latitude":-45.873394999999995,"speed":null,"altitudeAccuracy":null}},{"timestamp":1335700808000,"coords":{"heading":null,"altitude":null,"longitude":170.33427333333336,"accuracy":0,"latitude":-45.873711666666665,"speed":null,"altitudeAccuracy":null}},{"timestamp":1335700809000,"coords":{"heading":null,"altitude":null,"longitude":170.33488333333335,"accuracy":0,"latitude":-45.87475166666666,"speed":null,"altitudeAccuracy":null}}]');
+                }
             }),
             track: kendo.observable({
                 title: 'Track',
@@ -30,8 +35,7 @@
                         },
                         function (error) {
                             console.log(error);
-                        },
-                        {
+                        }, {
                             frequency: 3000,
                             enableHighAccuracy: true
                         });
@@ -66,7 +70,6 @@
                         zoom: 5,
                         mapTypeId: google.maps.MapTypeId.ROADMAP
                     };
-                    console.dir(document.getElementById("googleMap"));
                     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
                     google.maps.event.addDomListener(window, 'load', initialize);
                 },
@@ -96,7 +99,6 @@
         });
 
     }, false);
-
 
     //Events API
     function EventHandler() {}
